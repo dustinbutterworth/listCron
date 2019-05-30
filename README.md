3f2f05d370ebeea07c5b6775ef1687c25451f12f
Dustin Butterworth <dustin.butterworth@greenskycredit.com>
2019-05-30 09:59:18 -0400

    dlb - cleanup

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # listCron
 Quick and dirty shell script to gather cron information
+
+Creates a directory wherever the script is run.
+
+File/Directory info goes into ./listCron/lscron.txt
+Contents of cron go into ./listCron/cron.txt
--- listCron.sh
+++ listCron.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
-crontab -l > cron.out
-ls -alh /var/spool/cron >> lscron.txt
-ls -al /etc/ | grep cron >> lscron.txt
-ls -al /etc/cron* >> lscron.txt
-cat /etc/cron.d/* >> cron.txt
-cat /etc/at.allow >> cron.txt
-cat /etc/at.deny >> cron.txt
-cat /etc/cron.allow >> cron.txt
-cat /etc/cron.deny >> cron.txt
-cat /etc/crontab >> cron.txt
-cat /etc/anacrontab >> cron.txt
-cat /var/spool/cron/crontabs/root >> cron.txt
+DIRECTORY="./listCron"
+
+if [ ! -d $DIRECTORY ]; then
+  mkdir $DIRECTORY
+fi
+crontab -l > ./listCron/cron.txt
+ls -alh /var/spool/cron > ./listCron/lscron.txt
+ls -al /etc/ | grep cron >> ./listCron/lscron.txt
+ls -al /etc/cron* >> ./listCron/lscron.txt
+cat /etc/cron.d/* >> ./listCron/cron.txt
+cat /etc/at.allow >> ./listCron/cron.txt
+cat /etc/at.deny >> ./listCron/cron.txt
+cat /etc/cron.allow >> ./listCron/cron.txt
+cat /etc/cron.deny >> ./listCron/cron.txt
+cat /etc/crontab >> ./listCron/cron.txt
+cat /etc/anacrontab >> ./listCron/cron.txt
+cat /var/spool/cron/crontabs/root >> ./listCron/cron.txt
